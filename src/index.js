819bcb6478975c2ef9c309b215d9ddfbcec26fd2
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-28 18:44:53 +0200

    Style nav

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import 'normalize.css';
+import './index-style.css';
 import loadMainContent from './home-load.js';
 import loadMenuContent from './menu-load.js';
-import './index-style.css';
 
 const body = document.querySelector('body');
 
@@ -14,11 +15,11 @@ navItems.forEach(item => {
         main.classList.remove('home', 'menu', 'about', 'contact');
         main.textContent = '';
 
-        if (item.textContent === 'Home') {
+        if (item.dataset.index === '1') {
             main.classList.add('home');
             body.append(loadMainContent());
         }
-        else if(item.textContent === 'Menu') {
+        else if (item.dataset.index === '2') {
             main.classList.add('menu');
             body.append(loadMenuContent());
         };
